7f453f6e2679eb21a96146d94e999731c759ebea
Rakesh P <rakesh@fluidtouch.com>
2023-12-28 12:06:35 +0530

    Initial Commit

---
+++ EcommerceDemo/EcommerceDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceDemoApp.swift
+//  EcommerceDemo
+//
+//  Created by Rakesh on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
